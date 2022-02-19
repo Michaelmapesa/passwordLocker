7c37740d808ba3271f8486410004c32d619f096a
michael mapesa <michaelmapesa93@gmail.com>
2022-02-19 10:30:26 +0300

    add delete person method

--- passwordlock.py
+++ passwordlock.py
@@ -1,18 +1,22 @@
+from ast import Delete
 import string
 import random
 
-class person:
+class Person:
     person_list=[]
     def _init_(self,username,password):
        self.username = username
        self.password = password
 
     def _save_person(self):
-        person.person_list.append(self)
+        Person.person_list.append(self)
 
     @classmethod
     def dispay_person(cls):
         return cls.person_list
-        
+
+    def delete_person(self):
+        Person.person_list.remove(self)
+
 
     
