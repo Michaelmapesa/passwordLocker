2945a0d610fcf8cc474afbcb80c9161ec984d013
michael mapesa <michaelmapesa93@gmail.com>
2022-02-19 10:17:12 +0300

    creating user class and save

---
+++ passwordlock.py
@@ -0,0 +1,11 @@
+import string
+import random
+
+class person:
+    person_list=[]
+    def _init_(self,username,password):
+       self.username = username
+       self.password = password
+
+    def _save_person(self):
+        person.person_list.append(self)
