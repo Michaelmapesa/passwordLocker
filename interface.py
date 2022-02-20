949d1f61980c3e380fd5fdc63badd24b46c10f55
michael mapesa <michaelmapesa93@gmail.com>
2022-02-20 09:56:04 +0300

    creating credential interface functions

--- interface.py
+++ interface.py
@@ -1,3 +1,4 @@
+from enum import auto
 from tabnanny import check
 from passwordlock import Person,Credentials
 
@@ -18,6 +19,28 @@ def login_person(username,password):
 def create_new_credential(site,username,password):
     new_credential = Credentials(site,username,password)
     return new_credential
+
+def save_credentials(credentials):
+    credentials.save_details()
+
+def display_sites_details():
+    return Credentials.display_credentials()
+
+def delete_credentials(credentials):
+    credentials.delete_credentials()
+
+def verify_credential(site):
+    return Credentials.if_credential_exist(site)
+
+def generate_password():
+    auto_password=Credentials.generate_password()
+    return auto_password
+
+def copy_password(site):
+    return Credentials.copy_password(site)
+
+
+
 
 
 
